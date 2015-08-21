a0d0f0b1c1ba817800d3542d0a3f5fee8bb869bf
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-08-21 11:34:38 -0500

    Update tess-temp.js
    Here is a refactoring that I think you will like. (untested)

--- tess-temp.js
+++ tess-temp.js
@@ -1,359 +1,94 @@
 (function () {
 
-    'use strict';
+  'use strict';
 
-    var module = angular.module('tessTemp', []);
+  angular.module('material.layouts', ['material.core'])
+      .directive('layout'              , attributeDirective('layout'      , true)  )
+      .directive('layout-sm'           , attributeDirective('layout-sm'   , true)  )
+      .directive('layout-gt-sm'        , attributeDirective('layout-gt-sm', true)  )
+      .directive('layout-md'           , attributeDirective('layout-md'   , true)  )
+      .directive('layout-gt-md'        , attributeDirective('layout-gt-md', true)  )
+      .directive('layout-lg'           , attributeDirective('layout-lg'   , true)  )
+      .directive('layout-gt-lg'        , attributeDirective('layout-gt-lg', true)  )
+      .directive('flex'                , attributeDirective('flex'        , true)  )
+      .directive('flex-sm'             , attributeDirective('flex-sm'     , true)  )
+      .directive('flex-gt-sm'          , attributeDirective('flex-gt-sm'  , true)  )
+      .directive('flex-md'             , attributeDirective('flex-md'     , true)  )
+      .directive('flex-gt-md'          , attributeDirective('flex-gt-md'  , true)  )
+      .directive('flex-lg'             , attributeDirective('flex-lg'     , true)  )
+      .directive('flex-gt-lg'          , attributeDirective('flex-gt-md'  , true)  )
+      .directive('layout-align'        , attributeDirective('layout-align')        )
+      .directive('layout-align-sm'     , attributeDirective('layout-align-sm')     )
+      .directive('layout-align-gt-sm'  , attributeDirective('layout-align-gt-sm')  )
+      .directive('layout-align-md'     , attributeDirective('layout-align-md')     )
+      .directive('layout-align-gt-md'  , attributeDirective('layout-align-gt-md')  )
+      .directive('layout-align-lg'     , attributeDirective('layout-align-lg')     )
+      .directive('layout-align-gt-lg'  , attributeDirective('layout-align-gt-lg')  )
+      .directive('flex-order'          , attributeDirective('flex-order')          )
+      .directive('flex-order-sm'       , attributeDirective('flex-order-sm')       )
+      .directive('flex-order-gt-sm'    , attributeDirective('flex-order-gt-sm')    )
+      .directive('flex-order-md'       , attributeDirective('flex-order-md')       )
+      .directive('flex-order-gt-md'    , attributeDirective('flex-order-gt-md')    )
+      .directive('flex-order-lg'       , attributeDirective('flex-order-lg')       )
+      .directive('flex-order-gt-lg'    , attributeDirective('flex-order-gt-lg')    )
+      .directive('offset'              , attributeDirective('offset')              )
+      .directive('offset-sm'           , attributeDirective('offset-sm')           )
+      .directive('offset-gt-sm'        , attributeDirective('offset-gt-sm')        )
+      .directive('offset-md'           , attributeDirective('offset-md')           )
+      .directive('offset-gt-md'        , attributeDirective('offset-gt-md')        )
+      .directive('offset-lg'           , attributeDirective('offset-lg')           )
+      .directive('offset-gt-lg'        , attributeDirective('offset-gt-lg')        )
+      .directive('layout-padding'      , attributeDirective('layout-padding')      )
+      .directive('layout-wrap'         , attributeDirective('layout-wrap')         )
+      .directive('layout-fill'         , attributeDirective('layout-fill')         );
 
-    module.directive('layout', [function () {
-        return {
-            link: function (scope, element, attrs) {                
-                element.addClass('layout');
-                if (attrs.layout)
-                    element.addClass('layout-'+ attrs.layout);
-            }
-        };
-    }]);
+  /**
+   * To convert use of attribute selectors to class selectors,
+   * use a directive to do the work.
+   */
+  function attributeDirective( className, addDirectiveAsClass ) {
+      return [function() {
+          var directive = $normalize(className);
+          return {
+              /**
+               * Creates a postLink function for ngMaterial Layout attribute directive
+               * Note: This provides easy translation to switch ngMaterial
+               * attribute selectors to CLASS selectors and directives.
+               *
+               * !! This is important for IE Browser performance
+               *
+               * @param directive String like flextGtMd
+               * @param classname String like flex-gt-md
+               */
+              link: function(link, element, attrs) {
+                  if ( addDirectiveAsClass )  element.addClass(directive);
+                  if ( attrs[directive] )     element.addClass(directive + "-" + attrs[directive].replace(/ /g, "-"));
+              }
+          };
+      }];
+  }
 
-    module.directive('layoutSm', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                element.addClass('layout-sm');
-                if (attrs.layoutSm)
-                    element.addClass('layout-sm-' + attrs.layoutSm);
-            }
-        };
-    }]);
+  /**
+   * Converts all accepted directives format into proper directive name.
+   * @param name Name to normalize
+   */
+  function $normalize(name) {
+      return camelCase(name.replace(PREFIX_REGEXP, ''));
+  }
 
-    module.directive('layoutGtSm', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                element.addClass('layout-gt-sm');
-                if (attrs.layoutGtSm)
-                    element.addClass('layout-gt-sm-' + attrs.layoutGtSm);
-            }
-        };
-    }]);
+  var PREFIX_REGEXP = /^((?:x|data)[\:\-_])/i;
 
-    module.directive('layoutMd', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                element.addClass('layout-md');
-                if (attrs.layoutMd)
-                    element.addClass('layout-md-' + attrs.layoutMd);
-            }
-        };
-    }]);
+  /**
+   * Converts snake_case to camelCase.
+   * Also there is special case for Moz prefix starting with upper case letter.
+   * @param name Name to normalize
+   */
+  function camelCase(name) {
+      return name.
+              replace(SPECIAL_CHARS_REGEXP, function(_, separator, letter, offset) {
+                  return offset ? letter.toUpperCase() : letter;
+              }).
+              replace(MOZ_HACK_REGEXP, 'Moz$1');
+  }
 
-    module.directive('layoutGtMd', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                element.addClass('layout-gt-md');
-                if (attrs.layoutGtMd)
-                    element.addClass('layout-gt-md-' + attrs.layoutGtMd);
-            }
-        };
-    }]);
-
-    module.directive('layoutLg', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                element.addClass('layout-lg');
-                if (attrs.layoutLg)
-                    element.addClass('layout-lg-' + attrs.layoutLg);
-            }
-        };
-    }]);
-
-    module.directive('layoutGtLg', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                element.addClass('layout-gt-lg');
-                if (attrs.layoutGtLg)
-                    element.addClass('layout-gt-lg-' + attrs.layoutGtLg);
-            }
-        };
-    }]);
-
-    module.directive('flex', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                element.addClass('flex');
-                if (attrs.flex)
-                    element.addClass('flex-' + attrs.flex);
-            }
-        };
-    }]);
-
-    module.directive('flexSm', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                element.addClass('flex-sm');
-                if (attrs.flexSm)
-                    element.addClass('flex-sm-' + attrs.flexSm);
-            }
-        };
-    }]);
-
-    module.directive('flexGtSm', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                element.addClass('flex-gt-sm');
-                if (attrs.flexGtSm)
-                    element.addClass('flex-gt-sm-' + attrs.flexGtSm);
-            }
-        };
-    }]);
-
-    module.directive('flexMd', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                element.addClass('flex-md');
-                if (attrs.flexMd)
-                    element.addClass('flex-md-' + attrs.flexMd);
-            }
-        };
-    }]);
-
-    module.directive('flexGtMd', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                element.addClass('flex-gt-md');
-                if (attrs.flexGtMd)
-                    element.addClass('flex-gt-md-' + attrs.flexGtMd);
-            }
-        };
-    }]);
-
-    module.directive('flexLg', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                element.addClass('flex-lg');
-                if (attrs.flex)
-                    element.addClass('flex-lg-' + attrs.flex);
-            }
-        };
-    }]);
-
-    module.directive('flexGtLg', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                element.addClass('flex-gt-lg');
-                if (attrs.flex)
-                    element.addClass('flex-gt-lg-' + attrs.flex);
-            }
-        };
-    }]);
-
-    module.directive('layoutAlign', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.layoutAlign)
-                    element.addClass('layout-align-' + attrs.layoutAlign.replace(/ /g, "-"));
-            }
-        };
-    }]);
-
-    module.directive('layoutAlignSm', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.layoutAlignSm)
-                    element.addClass('layout-align-sm-' + attrs.layoutAlignSm.replace(/ /g, "-"));
-            }
-        };
-    }]);
-
-    module.directive('layoutAlignGtSm', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.layoutAlignGtSm)
-                    element.addClass('layout-align-gt-sm' + attrs.layoutAlignGtSm.replace(/ /g, "-"));
-            }
-        };
-    }]);
-
-    module.directive('layoutAlignMd', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.layoutAlignMd)
-                    element.addClass('layout-align-md-' + attrs.layoutAlignMd.replace(/ /g, "-"));
-            }
-        };
-    }]);
-
-    module.directive('layoutAlignGtMd', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.layoutAlignGtMd)
-                    element.addClass('layout-align-gt-md-' + attrs.layoutAlignGtMd.replace(/ /g, "-"));
-            }
-        };
-    }]);
-
-    module.directive('layoutAlignLg', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.layoutAlignLg)
-                    element.addClass('layout-align-lg-' + attrs.layoutAlignLg.replace(/ /g, "-"));
-            }
-        };
-    }]);
-
-    module.directive('layoutAlignGtLg', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.layoutAlignGtLg)
-                    element.addClass('layout-align-gt-lg' + attrs.layoutAlignGtLg.replace(/ /g, "-"));
-            }
-        };
-    }]);
-
-    module.directive('flexOrder', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.flexOrder)
-                    element.addClass('flex-order-' + attrs.flexOrder);
-            }
-        };
-    }]);
-
-    module.directive('flexOrderSm', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.flexOrderSm)
-                    element.addClass('flex-order-sm-' + attrs.flexOrderSm);
-            }
-        };
-    }]);
-
-    module.directive('flexOrderGtSm', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.flexOrderGtSm)
-                    element.addClass('flex-order-gt-sm' + attrs.flexOrderGtSm);
-            }
-        };
-    }]);
-
-    module.directive('flexOrderMd', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.flexOrderMd)
-                    element.addClass('flex-order-md-' + attrs.flexOrderMd);
-            }
-        };
-    }]);
-
-    module.directive('flexOrderGtMd', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.flexOrderGtMd)
-                    element.addClass('flex-order-gt-md' + attrs.flexOrderGtMd);
-            }
-        };
-    }]);
-
-    module.directive('flexOrderLg', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.flexOrderLg)
-                    element.addClass('flex-order-lg-' + attrs.flexOrderLg);
-            }
-        };
-    }]);
-
-    module.directive('flexOrderGtLg', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.flexOrderGtLg)
-                    element.addClass('flex-order-gt-lg-' + attrs.flexOrderGtLg);
-            }
-        };
-    }]);
-
-    module.directive('offset', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.offset)
-                    element.addClass('offset-' + attrs.offset);
-            }
-        };
-    }]);
-
-    module.directive('offsetSm', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.offsetSm)
-                    element.addClass('offset-sm-' + attrs.offsetSm);
-            }
-        };
-    }]);
-
-    module.directive('offsetGtSm', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.offsetGtSm)
-                    element.addClass('offset-gt-sm-' + attrs.offsetGtSm);
-            }
-        };
-    }]);
-
-    module.directive('offsetMd', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.offsetMd)
-                    element.addClass('offset-md-' + attrs.offsetMd);
-            }
-        };
-    }]);
-
-    module.directive('offsetGtMd', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.offsetGtMd)
-                    element.addClass('offset-gt-md-' + attrs.offsetGtMd);
-            }
-        };
-    }]);
-
-    module.directive('offsetLg', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.offsetLg)
-                    element.addClass('offset-lg-' + attrs.offsetLg);
-            }
-        };
-    }]);
-
-    module.directive('offsetGtLg', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                if (attrs.offsetGtLg)
-                    element.addClass('offset-gt-lg-' + attrs.offsetGtLg);
-            }
-        };
-    }]);
-
-    module.directive('layoutPadding', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                element.addClass('layout-padding');                
-            }
-        };
-    }]);  
-
-    module.directive('layoutWrap', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                element.addClass('layout-wrap');
-            }
-        };
-    }]);
-
-    module.directive('layoutFill', [function () {
-        return {
-            link: function (scope, element, attrs) {
-                element.addClass('layout-fill');
-            }
-        };
-    }]);
 })();
